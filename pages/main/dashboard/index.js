6ff1fc005fe025b01fbe1666b67f218c96e393a7
hituhin <hituhin09@gmail.com>
2022-09-16 18:19:54 +0600

    build nav for the project

--- pages/main/dashboard/index.js
+++ pages/main/dashboard/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Dashboard from '../../../components/main/dashboard/Dashboard'
+import Header from "../../../components/common/header/Header"
+import Layout from '../../../components/common/layouts/Layout';
 
 const index = () => {
     return (
         <div>
-            <Dashboard />
+            <Layout />
         </div>
     );
 };
