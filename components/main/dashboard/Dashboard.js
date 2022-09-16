77dae347a33117794db34a160740e7476ca3dfd3
hituhin <hituhin09@gmail.com>
2022-09-16 12:30:44 +0600

    Create Structure for this project

---
+++ components/main/dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            HAI ! THIS IS Dashboard!
+        </div>
+    );
+};
+
+export default Dashboard;
---
+++ pages/main/dashboard/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Dashboard from '../../../components/main/dashboard/Dashboard'
+
+const index = () => {
+    return (
+        <div>
+            <Dashboard />
+        </div>
+    );
+};
+
+export default index;
